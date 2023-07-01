9f536b678865a5487d53ce2cf1edcf45c2e3d6bb
anjali-2022 <anjali07pingale@gmail.com>
2023-07-01 12:45:36 +0530

    Update and rename floatadd.sh to addition.sh

--- addition.sh
+++ addition.sh
@@ -3,3 +3,8 @@ read -p "Enter first number:" e
 read -p "Enter second number:" f
 r=$(bc<<<"scale=2;$e+$f")
 echo "the addition is $r"
+read -p "enter integer value1:" a
+read -p "enter integer value2:" b
+
+c=$((a+b))
+echo "addition of integer is $c"
