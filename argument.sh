27c9bf7f09a22d798234d70f2461a43a6b60f305
anjali-2022 <anjali07pingale@gmail.com>
2023-07-01 12:42:13 +0530

    Create argument.sh

---
+++ argument.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#print 1st argument
+echo $1
+echo $2
+echo $3
+#print 10th argument
+echo ${10}
+#print no of argument
+echo $#
+#print all arguments
+echo $*
+#print all arguments
+echo $@
+# print 0th argument i.e script name
+echo $0
